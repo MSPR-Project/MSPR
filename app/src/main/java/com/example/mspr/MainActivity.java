8ba7212cbe2d33f71cba150b2e47d711a7cd4d45
wabla24 <wabla51@gmail.com>
2020-03-30 12:41:14 +0200

    camera photo + debut stockage photo firebase

--- app/src/main/java/com/example/mspr/MainActivity.java
+++ app/src/main/java/com/example/mspr/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.mspr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    FirebaseStorage storage;
+    StorageReference storageRef,imageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //accessing the firebase storage
+        storage = FirebaseStorage.getInstance();
+
+        //creates a storage reference
+        storageRef = storage.getReference();
+
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void dispatchTakePictureIntent(View view) {
+        System.out.println("test camera fonction");
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
