1b88c1d83a2e830d783062c83611b95ad97e80ca
libbrecht <36694050+libbrecht@users.noreply.github.com>
2020-04-11 18:05:14 +0200

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,6 +4,10 @@ node{
 		stage('PremiereEtape'){
 			sh "echo 'hello wordl'"
 		}
+		stage('UnitTest'){
+			sh "javac /app/src/test/java/com/example/mspr/ExampleUnitTest.java"
+			sh "java ExampleUnitTest"
+		}
 	}
 	finally{
 		cleanWs()
