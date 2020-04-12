ee86033b153af429297e02a976b93486b19d2210
Frazia <Fabien.6k@gmail.com>
2020-04-12 16:25:25 +0200

    Lib Firebase added, config in progress / Gui in progress (Second page added with checkbox & submit button)

--- src/main/java/Gui/Camera.java
+++ src/main/java/Gui/Camera.java
@@ -12,6 +12,8 @@ import org.opencv.imgcodecs.Imgcodecs;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
@@ -31,7 +33,26 @@ import java.util.stream.Stream;
 import static javax.imageio.ImageIO.read;
 
 public class Camera extends JFrame {
+
+    static JFrame frame = new JFrame("GoSecuri");
+
     static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+    JButton startStream = new JButton("Start");
+    JCheckBox cb1 = new JCheckBox("Mousqueton");
+    JCheckBox cb2 = new JCheckBox("Gants d'intervention");
+    JCheckBox cb3 = new JCheckBox("Ceinture de sécurite tactique");
+    JCheckBox cb4 = new JCheckBox("Detecteur de metaux");
+    JCheckBox cb5 = new JCheckBox("Brassard de securite");
+    JCheckBox cb6 = new JCheckBox("Lampe torche");
+    JCheckBox cb7 = new JCheckBox("Bandeau Agent cynophile");
+    JCheckBox cb8 = new JCheckBox("Gilet pare-balle");
+    JCheckBox cb9 = new JCheckBox("Chemise manches courtes");
+    JCheckBox cb10 = new JCheckBox("Blouson");
+    JCheckBox cb11 = new JCheckBox("Coupe-vent");
+    JCheckBox cb12 = new JCheckBox("Talkie-Walkie");
+    JCheckBox cb13 = new JCheckBox("Kit oreillette");
+    JCheckBox cb14 = new JCheckBox("Taser");
+
 
     class DaemonThread implements Runnable{
         protected volatile boolean runnable = false;
@@ -47,9 +68,9 @@ public class Camera extends JFrame {
                     {
                         try
                         {
-                            webSource.retrieve(frame);
-                            if ( frame.empty() ) continue;
-                            Imgcodecs.imencode(".bmp", frame, mem);
+                            webSource.retrieve(pol);
+                            if ( pol.empty() ) continue;
+                            Imgcodecs.imencode(".bmp", pol, mem);
                             Image im = ImageIO.read(new ByteArrayInputStream(mem.toArray()));
 
                             BufferedImage buff = (BufferedImage) im;
@@ -60,7 +81,7 @@ public class Camera extends JFrame {
                                 if(runnable == false)
                                 {
                                     webSource.release();
-                                    System.out.println("Going to wait()");
+                                    System.out.println("Runnable = false so we stop the webcam");
                                     this.wait();
                                 }
                         }
@@ -74,22 +95,17 @@ public class Camera extends JFrame {
         }
     }
     public JPanel MainPanelAuth;
-    private JButton buttonStart;
     private JPanel CameraPanel;
     private JButton buttonCapture;
 
     private DaemonThread myThread = null;
     int count = 0;
     VideoCapture webSource = null;
-    Mat frame = new Mat();
+    Mat pol = new Mat();
     MatOfByte mem = new MatOfByte();
 
 
     public Camera() {
-                buttonStart.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
                 webSource =new VideoCapture(0);
                 webSource.open(0);
                 myThread = new DaemonThread();
@@ -97,27 +113,57 @@ public class Camera extends JFrame {
                 t.setDaemon(true);
                 myThread.runnable = true;
                 t.start();
-                buttonStart.setEnabled(false);  //start button
-                buttonCapture.setEnabled(true);  // stop button
-            }
-        });
+                buttonCapture.setEnabled(true);  // capture button
 
             buttonCapture.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
+
                 myThread.runnable = false;
                 buttonCapture.setEnabled(false);
-                buttonStart.setEnabled(true);
+                CameraPanel.setVisible(false);
+                CameraPanel.setVisible(false);
+                webSource.release();
+                formMain();
+
             }
         });
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Go securi");
+        setupFrame();
+    }
+
+    public void formMain(){
+        JPanel panelForm = new JPanel();
+        panelForm.setVisible(true);
+        frame.setContentPane(panelForm);
+        panelForm.setBackground(Color.LIGHT_GRAY);
+        panelForm.add(startStream);
+        panelForm.add(cb1);
+        panelForm.add(cb2);
+        panelForm.add(cb3);
+        panelForm.add(cb4);
+        panelForm.add(cb5);
+        panelForm.add(cb6);
+        panelForm.add(cb7);
+        panelForm.add(cb8);
+        panelForm.add(cb9);
+        panelForm.add(cb10);
+        panelForm.add(cb11);
+        panelForm.add(cb12);
+        panelForm.add(cb13);
+        panelForm.add(cb14);
+
+    }
+
+    public static void setupFrame(){
+
+        frame.setSize(1000,700);
         frame.setContentPane(new Camera().MainPanelAuth);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
+        //frame.pack();
         frame.setVisible(true);
     }
 }
