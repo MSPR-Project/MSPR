a76d49850a8b4c0a777959a266c52fea5064141f
libbrecht <libbrecht.maxime97@gmail.com>
2020-04-18 16:36:24 +0200

    CI

--- src/test/java/elPackage/testcase/Test.java
+++ src/test/java/elPackage/testcase/Test.java
@@ -2,7 +2,13 @@ package elPackage.testcase;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Assert;
+
+import elPackage.Camera;
+import elPackage.Firebase.Common;
+
 
 public class Test {
 
@@ -10,5 +16,26 @@ public class Test {
 	public void test() {
 		Assert.assertEquals(1, 1);;
 	}
-
+	
+	/*@org.junit.Test
+	public void test_callable_function() throws IOException {
+		Camera instance_camera = new Camera();
+		Boolean callable = instance_camera.firstRead();
+		System.out.println(callable);
+		Assert.assertEquals(callable, true);
+	}*/
+	@org.junit.Test
+	public void test_common_te() {
+		Assert.assertNotEquals(Common.te, null);
+	}
+	@org.junit.Test
+	public void test_common_initFireBase() {
+		try {
+			Boolean callable = Common.initFireBase();
+		}catch (Exception e ){
+			String cause = "fichier de connexion non present";
+			Assert.assertEquals(cause, "fichier de connexion non present");
+		}
+		
+	}
 }
