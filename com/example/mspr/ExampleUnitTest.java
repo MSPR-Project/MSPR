e44ab4d5afb8b5529b837107753f261493d0ea39
libbrecht <libbrecht.maxime97@gmail.com>
2020-04-19 14:28:33 +0200

    CI

--- com/example/mspr/ExampleUnitTest.java
+++ com/example/mspr/ExampleUnitTest.java
@@ -3,7 +3,7 @@ package com.example.mspr;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-
+import main.java.com.exemple.mspr.MainActivity
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -14,6 +14,19 @@ public class ExampleUnitTest {
     public void test_REQUEST_IMAGE_CAPTURE(){
         int test_REQUEST_IMAGE_CAPTURE = 1;
         int true_value = MainActivity.REQUEST_IMAGE_CAPTURE;
-        assertEquals(test_REQUEST_IMAGE_CAPTURE, true_value);
+        Assert.assertEquals(test_REQUEST_IMAGE_CAPTURE, true_value);
+    }
+
+    @Test
+    public void test_runTextRecognition(){
+        MainActivity instance = new MainActivity();
+        Boolean flag = false;
+        try:
+            instance.runTextRecognition()
+            flag = true;
+        catch (Exception e ):
+            flag = false;
+        Assert.assertEquals(true, flag);
+
     }
 }
